b577511f5b992a40af20a5c16765f3f90007d479
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-10-03 17:03:45 +0300

    tusk3

---
+++ src/vsu/cs/tusk3/Enum.java
@@ -0,0 +1,13 @@
+package vsu.cs.tusk3;
+
+public class Enum {
+
+    public enum SimpleColor {
+        YELLOW,
+        ORANGE,
+        GREEN,
+        WHITE;
+    }
+
+}
+
---
+++ src/vsu/cs/tusk3/Main.java
@@ -0,0 +1,49 @@
+package vsu.cs.tusk3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static final Line L1 = new Line(0, 3, 0);
+    public static final VerticalParabola V1 = new VerticalParabola(5, 1, -1);
+    public static final Circle C1 = new Circle(-6, 4, 2);
+
+
+    public static void main(String[] args) {
+
+        printColorForPoint(5,9);
+        printColorForPoint(6,-5);
+        printColorForPoint(5,0.5);
+        printColorForPoint(-6, 2.5);
+        printColorForPoint(-5,4);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input x = ");
+        double x = sc.nextDouble();
+        System.out.print("Input y = ");
+        double y = sc.nextDouble();
+        System.out.printf("(%.1f, %.1f) -> %S%n", x, y, getColor(x,y));
+    }
+
+    public static Enum.SimpleColor getColor(double x, double y) {
+
+        if (L1.isPointAboveLine(x, y) && !C1.isPointInsideCircle(x, y)) {
+            return Enum.SimpleColor.YELLOW;
+        }
+
+        if (C1.isPointInsideCircle(x, y) && L1.isPointAboveLine(x, y)
+                || (!L1.isPointAboveLine(x, y) && !C1.isPointInsideCircle(x, y) && !V1.isPointInsideParabola(x, y))) {
+            return Enum.SimpleColor.WHITE;
+        }
+
+        if (C1.isPointInsideCircle(x, y) && !L1.isPointAboveLine(x,y)) {
+            return Enum.SimpleColor.ORANGE;
+        }
+        return  Enum.SimpleColor.GREEN;
+    }
+
+    public static void printColorForPoint(double x, double y){
+        System.out.printf("(%.1f, %.1f) -> %S%n", x, y, getColor(x,y));
+    }
+
+}
